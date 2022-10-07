b2d3411afee929ff6cac6e26d63be7eda3a3864c
carOrLa03 <carmenorla@icloud.com>
2022-10-07 12:11:58 +0200

    carmenSanDiego

--- carmenSanDiego/ejer1/scripts1.js
+++ carmenSanDiego/ejer1/scripts1.js
@@ -1,18 +1,36 @@
+const form = document.getElementById('form');
+// form.addEventListener("submit", (e)=>{
+//   e.preventDefault();
+
+//   const nombre = document.getElementById('nombre').value;
+//   const apellidos = document.getAnimations('apellidos').value;
+//   const fecha = document.getElementById('nacimiento').value;
+//   const userName = nombre.slice(0,1) + apellidos.slice(0,apellidos.length())+ fecha.slice(fecha.length()-2);
   
-var nombre = document.getElementById('nombre');
-var apellidos = document.getAnimations('apellidos');
-var fecha = document.getElementById('nacimiento');
-var userName = nombre.slice(0,1) + apellidos.slice(0,apellidos.length())+ fecha.slice(fecha.length()-2);
+//   const caja = document.getElementById("caja")
+//   const miNombre = "<p> Nombre: " + nombre + "</p>"
+//   const miApellido = "<p> Apellido: "+ apellidos + "<p>"
+//   const miFecha = "<p> Fecha de Nacimiento: " + fecha + "</p>"
+//   const miUsername = "<p> User name:" +  userName + "<p>"
+  
+//   caja.innerHTML = miNombre + miApellido + miFecha + miUsername;
+// });
 
-
-  function escribe() {
-    let escribir = document.getElementById("caja")
-    let miNombre = "<p> Nombre: " + nombre + "</p>"
-    let miApellido = "<p> Apellido: "+ apellidos + "<p>"
-    let miFecha = "<p> Fecha de Nacimiento: " + fecha + "</p>"
-    let miUsername = "<p> User name:" +  username + "<p>"
-    escribir.innerHTML = miNombre + miApellido + miFecha + miUsername;
-    }
-window.onload = function() {
-document.form.Enviar.click = escribe()
+form.addEventListener("click", escribe);
+function escribe(e){
+  e.preventDefault();
+  const nombre = document.getElementById('nombre');
+  const apellidos = document.getElementById('apellidos');
+  const fecha = document.getElementById('nacimiento');
+  const userName = nombre.slice(0,1) + apellidos.slice(0,apellidos.length())+ fecha.slice(fecha.length()-2);
+  
+  const caja = document.getElementById("caja")
+  const miNombre = "<p> Nombre: " + nombre + "</p>"
+  const miApellido = "<p> Apellido: "+ apellidos + "<p>"
+  const miFecha = "<p> Fecha de Nacimiento: " + fecha + "</p>"
+  const miUsername = "<p> User name:" +  userName + "<p>"
+  
+  caja.innerHTML = miNombre + miApellido + miFecha + miUsername;
 }
+
+
