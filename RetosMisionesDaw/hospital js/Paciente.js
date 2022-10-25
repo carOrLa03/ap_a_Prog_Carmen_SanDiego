6c9fc45fc20f3c8da73918b538f37b27cfa2c470
CarOrLa03 <carmenorla03@gmail.com>
2022-10-25 21:39:01 +0200

    carmen san diego

--- RetosMisionesDaw/hospital js/Paciente.js
+++ RetosMisionesDaw/hospital js/Paciente.js
@@ -1,44 +1,41 @@
-export default class Paciente{
-    nombre;
-    tlf;
-    dni;
-    // especialista = especialista.getDni();
+export default class Paciente {
+  nombre;
+  tlf;
+  dni;
+  especialista;
 
-    constructor(nom, tlf, dni){
-        this.nombre = nom;
-        this.tlf = tlf;
-        this.dni = dni;
-        
-    }
+  constructor(nom, tlf, dni) {
+    this.nombre = nom;
+    this.tlf = tlf;
+    this.dni = dni;
+    this.especialista = "";
+  }
 
-    get nombre(){
-        return this.nombre;
-    }
-    set nombre(nom){
-        this.nombre = nom;
-    }
-    get telefono(){
-        return this.tlf;
-    }
-    set telefono(tlf){
-        this.tlf = tlf;
-    }
-    get dni(){
-        return this.dni;
-    }
-    set nombre(dni){
-        this.dni = dni;
-    }
-    get especialista() {
-        return this.especialista;
-    }
-    set especialista(especialista) {
-        this.especialista = especialista.getDni();
-    }
-    muestraDatos() {
-        return `DNI del paciente: ${this.dni}\n Nombre: ${this.nombre}\n Télefono: ${this.tlf}\n Especialista asignado: ${this.especialista}`;
-    }
+  get nombre() {
+    return this.nombre;
+  }
+  set nombre(nom) {
+    this.nombre = nom;
+  }
+  get telefono() {
+    return this.tlf;
+  }
+  set telefono(tlf) {
+    this.tlf = tlf;
+  }
+  get dni() {
+    return this.dni;
+  }
+  set nombre(dni) {
+    this.dni = dni;
+  }
+  get especialista() {
+    return this.especialista;
+  }
+  set especialista(especialista) {
+    this.especialista = especialista.getDni();
+  }
+  muestraDatos() {
+    return `DNI del paciente: ${this.dni}\n Nombre: ${this.nombre}\n Télefono: ${this.tlf}\n Especialista asignado: ${this.especialista}`;
+  }
 }
-paciente1 = new Paciente("carmen", 962502455, "7357770J")
-console.log(paciente1.muestraDatos())
-console.log("hola")
